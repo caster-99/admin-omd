6516b9dec77989936b13d4a5f67c2fa09ee018bf
Sarai Gabriela <103385050+saraiherrerab@users.noreply.github.com>
2026-02-23 18:56:49 -0400

    Correcciones sintacticas

--- src/hooks/debug_tx.ts
+++
@@ -1,4 +0,0 @@
-
-console.log('DEBUG: Transaction Response', responseData);
-console.log('DEBUG: Keys in response', Object.keys(responseData));
-if (responseData.meta) console.log('DEBUG: Meta', responseData.meta);
